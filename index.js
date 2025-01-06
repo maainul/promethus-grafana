b12d5985205e040c2e785aa2ebb9085e774ef25f
maainul <mainul080@yahoo.com>
2025-01-06 16:33:56 +0600

    update code

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const client = require("prom-client")
 const express = require("express")
 require('dotenv').config();
-
+const responseTime = require("response-time")
 
 const app = express()
 
 // Default Metrics Collection
 client.collectDefaultMetrics({ register: client.register });
 
+const reqResTime = new client.Histogram({
+    name:'http_express_req_res_time',
+    help:'This tells how much ime is taken by req and res',
+    labelNames:["method","route","status_code"],
+    buckets:[1,50,100,200,400,500,800,1000,2000]
+})
+app.use(responseTime((req,res,time)=>{
+    reqResTime.labels({
+        method:req.method,
+        route:req.url,
+        status_code:res.statusCode
+    }).observe(time)
+}))
 
 app.get("/",async(req,res)=>{
     return res.json({
